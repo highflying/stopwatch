e8146d6a45d031e13fb982d5b2fd8fa90ec6c006
Ben Rogers <ben@bdr.org>
2013-11-01 09:08:33 +0000

    Improved test_watcher.pl from similar script

--- test_watcher.pl
+++ test_watcher.pl
@@ -6,6 +6,7 @@ use utf8;
 
 use App::Prove;
 use File::ChangeNotify::Watcher::Default;
+use List::MoreUtils qw( distinct );
 
 my @paths = @ARGV;
 
@@ -21,8 +22,9 @@ sub watch_for_changes
     my (@paths) = @_;
 
     my $watcher = File::ChangeNotify::Watcher::Default->new(
-        directories => \@paths,
-        filter      => qr/ [.] (pl|pm|t) \z /sxm,
+        directories     => \@paths,
+        filter          => qr/ [.] (pl|pm|t) \z /sxm,
+        follow_symlinks => 1,
     );
 
     print STDERR "#################### Running Tests ####################\n\n";
@@ -33,6 +35,10 @@ sub watch_for_changes
 
     while ( my @events = $watcher->wait_for_events )
     {
+        sleep 1;
+
+        push @events, $watcher->new_events;
+
         print STDERR "\nChanges found...\n\n";
 
         for my $event (@events)
@@ -40,9 +46,33 @@ sub watch_for_changes
             print STDERR 'File changed: ', $event->path, ' (', $event->type, ")\n";
         }
 
-        print STDERR "\n#################### Re-Running Tests ####################\n\n";
+        my @code_paths = distinct map { $_->path }
+          grep { $_->path =~ / [.] (?:pl|pm) \z /sxm and $_->type ne 'delete' } @events;
 
-        run_tests();
+        my $run_tests = 1;
+
+        if (@code_paths)
+        {
+            print STDERR
+              "\n#################### Checking Syntax ####################\n\n";
+
+            for my $code_file (@code_paths)
+            {
+                my $rc = system( 'perl', '-c', $code_file );
+                if ($rc)
+                {
+                    $run_tests = 0;
+                }
+            }
+        }
+
+        if ($run_tests)
+        {
+            print STDERR
+              "\n#################### Re-Running Tests ####################\n\n";
+
+            run_tests();
+        }
 
         print STDERR "\n#################### Watching for Changes ####################\n";
     }
@@ -50,14 +80,42 @@ sub watch_for_changes
 
 sub run_tests
 {
-    if ( fork == 0 )
+    my $child_pid = fork;
+
+    if ( $child_pid == 0 )
     {
         my $app = App::Prove->new;
         $app->process_args( '-v', 't' );
-        exit( $app->run ? 0 : 1 );
+        my $rc = $app->run ? 0 : 1;
+
+        if ($rc)
+        {
+
+            print STDERR <<FAIL;
+
+           FFFFF   A    III L
+           F      A A    I  L
+           F     A   A   I  L
+           FFFF A     A  I  L
+           F    AAAAAAA  I  L
+           F    A     A  I  L
+           F    A     A III LLLLL
+
+FAIL
+        }
+
+        exit($rc);
     }
 
+    local $SIG{INT} = sub
+    {
+        kill 9, $child_pid;
+        exit;
+    };
+
     wait;
+
+    local $SIG{INT} = 'DEFAULT';
 
     return;
 }
